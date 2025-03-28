55e1f94c8fd9cb29a25500f6078605a2df30cb42
Prasanjeet-Microsoft <v-singhprasa@microsoft.com>
2025-03-28 13:44:05 +0530

    Enhance deployment: Add .dockerignore and optimize PowerShell script for cross-OS support

--- Deployment/resourcedeployment.ps1
+++ Deployment/resourcedeployment.ps1
@@ -917,32 +917,32 @@ try {
     # 2. Build and push the images to Azure Container Registry
     #  2-1. Build and push the AI Service container image to  Azure Container Registry
     #$acrAIServiceTag = "$($deploymentResult.AzContainerRegistryName).azurecr.io/$acrNamespace/aiservice"
-    docker build ..\App\backend-api\. --no-cache -t $acrAIServiceTag
+    docker build "../App/backend-api/." --no-cache -t $acrAIServiceTag
     docker push $acrAIServiceTag
 
     #  2-2. Build and push the Kernel Memory Service container image to Azure Container Registry
     #$acrKernelMemoryTag = "$($deploymentResult.AzContainerRegistryName).azurecr.io/$acrNamespace/kernelmemory"
-    docker build ..\App\kernel-memory\. --no-cache -t $acrKernelMemoryTag
+    docker build "../App/kernel-memory/." --no-cache -t $acrKernelMemoryTag
     docker push $acrKernelMemoryTag
 
     #  2-3. Build and push the Frontend App Service container image to Azure Container Registry
     #$acrFrontAppTag = "$($deploymentResult.AzContainerRegistryName).azurecr.io/$acrNamespace/frontapp"
-    docker build ..\App\frontend-app\. --no-cache -t $acrFrontAppTag
+    docker build "../App/frontend-app/." --no-cache -t $acrFrontAppTag
     docker push $acrFrontAppTag
 
 #======================================================================================================================================================================
 
     # 7.2. Deploy ClusterIssuer in Kubernetes for SSL/TLS certificate
-    kubectl apply -f .\kubernetes\deploy.certclusterissuer.yaml
+    kubectl apply -f "./kubernetes/deploy.certclusterissuer.yaml"
 
     # 7.3. Deploy Deployment in Kubernetes
-    kubectl apply -f .\kubernetes\deploy.deployment.yaml -n $kubenamespace
+    kubectl apply -f "./kubernetes/deploy.deployment.yaml" -n $kubenamespace
 
     # 7.4. Deploy Services in Kubernetes
-    kubectl apply -f .\kubernetes\deploy.service.yaml -n $kubenamespace
+    kubectl apply -f "./kubernetes/deploy.service.yaml" -n $kubenamespace
 
     # 7.5. Deploy Ingress Controller in Kubernetes for external access
-    kubectl apply -f .\kubernetes\deploy.ingress.yaml -n $kubenamespace
+    kubectl apply -f "./kubernetes/deploy.ingress.yaml" -n $kubenamespace
 
     # #####################################################################
     # # Data file uploading
